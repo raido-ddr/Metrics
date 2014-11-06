97f9d751c2e8511c8a3de509e44d2c717d325903
raido-ddr <raido.ddr@gmail.com>
2014-11-06 10:36:24 +0300

    Implement Strategy pattern

---
+++ Metrics/MetricEvaluationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics
+{
+    class MetricEvaluationContext
+    {
+        private MetricEvaluationStrategy Strategy { get; set; }
+
+        public MetricEvaluationContext(MetricEvaluationStrategy strategy)
+        {
+            Strategy = strategy;
+        }
+
+        public Metric EvaluateMetric(AnalyzableSource sourceCode)
+        {
+            return Strategy.EvaluateMetric(sourceCode);
+        }
+    }
+}
